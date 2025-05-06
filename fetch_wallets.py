ff47b3632e912040f5936c05a9e1ee1b97fc6036
b4mz <87509002+bamphd@users.noreply.github.com>
2025-05-07 00:10:15 +0700

    Create fetch_wallets.py

---
+++ fetch_wallets.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://api.socialscan.io/rest/monad-testnet/v2/explorer/token/0x922da3512e2bebbe32bcce59adf7e6759fb8cea2/top_holders?page=1&size=50"
+response = requests.get(url)
+data = response.json()
+
+wallets = [holder["wallet_address"] for holder in data.get("data", [])]
+
+with open("erc20_wallets.txt", "w") as f:
+    for wallet in wallets:
+        f.write(wallet + "\n")
